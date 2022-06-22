205f69693b9a68ffa46c38db6edf681c48960546
fabinhoizzy <fabinhoevil16@hotmail.com>
2022-06-22 19:44:18 -0300

    criando o cronometro com os metodos iniciar e finalizar

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -8,16 +8,16 @@
             <div class="is-flex is-align-items-center is-justify-content-space-between">
                 
                 <section>
-                    <strong>00:00:00</strong>
+                    <strong>{{ tempoDecorrido }}</strong>
                  </section>    
-                    <button class="button">
+                    <button class="button" @click="iniciar">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>play</span>
     
                     </button>
-                    <button class="button">
+                    <button class="button" @click="finalizar">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -34,7 +34,29 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'Formulario'
+    name: 'Formulario',
+    data () {
+        return {
+            tempoEmSegundo: 0,
+            cronometro: 0
+        }
+    },
+    computed: {
+        tempoDecorrido() : string {
+            return new Date(this.tempoEmSegundo * 1000).toISOString().substring(19, 11);
+        }
+    },
+    methods: {
+        iniciar() {
+            this.cronometro = setInterval(()=> {
+                this.tempoEmSegundo += 1
+            }, 1000)    
+        },
+        finalizar() {
+            clearInterval(this.cronometro)
+       }
+
+    }
 })
 
 </script>
