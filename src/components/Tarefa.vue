c00a34f0164bb7e5cf7e6ccd30926ee2eb4f87d0
fabinhoizzy <fabinhoevil16@hotmail.com>
2022-06-25 11:18:19 -0300

    Adicionando a primeira rota ao projeto

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,33 +1,31 @@
 <template>
-<div class="box has-text-weight-bold">
+  <Box>
     <div class="columns">
-       <div class="column is-7"> {{tarefa.descricao || 'Tarefa sem descrição'}} </div>
+      <div class="column is-7">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
+      <div class="column">
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+      </div>
     </div>
-    <div class="column">
-        <Cronometro :tempoEmSegundo="tarefa.duracaoEmSegundos"/>
-    </div>
-</div>
+  </Box>
 </template>
 
 <script lang="ts">
-
-import ITarefa from "../interfaces/ITarefa";
 import { defineComponent, PropType } from "vue";
 import Cronometro from "./Cronometro.vue";
+import ITarefa from '../interfaces/ITarefa'
+import Box from './Box.vue'
 
 export default defineComponent({
-    name:  "Tarefa",
-    components: {
-        Cronometro,
-    },
-    props: {
-        tarefa: {
-            type: Object as PropType<ITarefa>,
-            required: true
-        }
+  name: "Tarefa",
+  components: {
+    Cronometro,
+    Box
+  },
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true
     }
+  }
 });
 </script>
-
-<style>
-</style>
