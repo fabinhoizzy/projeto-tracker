307367498fcef2b8919b2ffc36a1b750354ff734
fabinhoizzy <fabinhoevil16@hotmail.com>
2022-06-29 19:50:51 -0300

    Adicionando mensagem de sucesso ao ÃusÃuário, quando o mesmo cria um projeto

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -1,7 +1,5 @@
 <template>
-  <section class="projetos">
-    <h1 class="title">Projetos</h1>
-
+  <section>
   <form @submit.prevent="salvar">
     <div class="field">
       <label for="nomeDoProjeto" class="label">
@@ -22,6 +20,8 @@
 <script lang="ts">
 import {defineComponent} from "vue";
 import {useStore} from "@/store";
+import {ADICIONA_PROJETO, ALTERA_PROJETO, NOTIFICAR} from "@/store/tipo-mutacoes";
+import {TipoNotificacao} from "@/interfaces/INotificacao";
 
 export default defineComponent({
   name: 'Formulario',
@@ -44,14 +44,19 @@ export default defineComponent({
   methods: {
     salvar() {
       if (this.id) {
-        this.store.commit('ALTERA_PROJETO', {
+        this.store.commit(ALTERA_PROJETO, {
           id: this.id,
           nome: this.nomeDoProjeto
         })
       } else {
-        this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+        this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
       }
       this.nomeDoProjeto = ""
+      this.store.commit(NOTIFICAR, {
+        titulo: 'Novo projeto foi salvo',
+        texto: 'Prontinho, seu projeto já está disponível.',
+        tipo: TipoNotificacao.SUCESSO
+      })
       this.$router.push('/projetos')
     }
   },
@@ -63,9 +68,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style scoped>
-.projetos {
-    padding: 1.2rem;
-  }
-</style>
